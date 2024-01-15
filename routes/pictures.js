1aceba4f3605413b7f47c42dffb907323497bffb
MichVogt <mvogt1995@gmail.com>
2024-01-15 12:24:49 +0100

    Task 1 complete

--- routes/pictures.js
+++ routes/pictures.js
@@ -5,12 +5,16 @@ var path = require('path');
 
 // Or
 /* GET pictures listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
-  res.render('pictures', { pictures: pictures});
+  res.render('pictures', { pictures: pictures });
 });
 
-router.post('/', function(req, res, next) {
+router.get('/:pictureName', function (req, res, next) {
+  res.render('pictureDetails', { picture: req.params.pictureName })
+});
+
+router.post('/', function (req, res, next) {
   const file = req.files.file;
   fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
   res.end();
